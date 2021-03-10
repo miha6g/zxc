ab8fa34474ef82e4a9485acbf13481488fb1c104
miha6g <72049060+miha6g@users.noreply.github.com>
2021-03-10 19:49:48 +0300

    Update main6.pas

--- main6.pas
+++ main6.pas
@@ -1,21 +1,24 @@
 program Hello;
-Var i, j :integer; s, sum, c, e :real;
+uses Math;
+Var i, j :integer; s, sum, c, e, FakJ :real;
 begin
   i := 1;
   sum := 0;
   for i:=1 to 20 do
   begin
         j := 1;
-        e := sqrt(i+j)/j;
+        FakJ := 1;
         c := ln(i);
         s := 0;
-        while e > 0.0001 do 
+        e := 1;
+        while abs(e) > 0.0001 do 
         begin
+            e := (sqrt(i+j))/FakJ;
             s := s + e;
             inc(j);
-            e := e/j;
+            FakJ := FakJ*j;
         end;
-        sum := sum + s*ln(i);
+        sum := sum + (s*ln(i));
   end;
   s := 2.7*sum;
 writeln(s:0:4)
